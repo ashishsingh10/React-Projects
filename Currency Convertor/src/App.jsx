924b52e2a16bbb705d2544b77f7bafaa9f8b9833
Ashish Singh <ashish.singh24785@gmail.com>
2023-10-10 21:35:43 +0530

    updating repo

--- Currency Convertor/src/App.jsx
+++ Currency Convertor/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-
+import useCurrencyInfo from './hooks/useCurrencyinfo.js'
+import InputBox from './components/InputBox'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className="text-3xl bg-blue-500">Currency Convertor</h1>
+      <useCurrencyInfo />
+      <InputBox />
     </>
   )
 }
