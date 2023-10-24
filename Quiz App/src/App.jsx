2b2ae1112eae55283169fde13ce337d9bc77c54e
Ashish Singh <ashish.singh24785@gmail.com>
2023-10-24 19:52:22 +0530

    updating repo

--- Quiz App/src/App.jsx
+++ Quiz App/src/App.jsx
@@ -1,27 +1,86 @@
-import React from 'react'
-import './App.css'
-import { QuizData } from '../Data/QuizData'
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import { QuizData } from "./Data/QuizData";
+import QuizResult from "./components/QuizResult.jsx";
 
 function App() {
-console.log("hi", QuizData)
-  return (
-    <>
-      <h1>Quiz App</h1>
-      <div className="container">
-        
+  // console.log(QuizData.length,
+  //   "hi", QuizData)
 
-        <p>hi</p>
-        <div className="question">{QuizData.map((element) => {
-          return element.question
-        })}</div>
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [clickedOption, setClickedOption] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  // const endQuiz = () => {
+  //   return <h1>end</h1>
+  //   console.log("end 1")
+  // }
+  function clickAction() {
+    selectAction();
+    if (currentQuestion < QuizData.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+      setClickedOption(0)
+    } else {
+      setShowResult(true)
+    }
+    
+    // setClickedOption(0);
 
-        
+    // let index = 0;
+    // let total = QuizData.length
+  }
 
+  // if (currentQuestion == QuizData.length) {
+  //   setShowResult(true);
+  // }
 
-      </div>
-      
-    </>
-  )
+  const selectAction = () => {
+    if (clickedOption === QuizData[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+  };
+
+  const resetAll=()=>{
+    setShowResult(false);
+    setCurrentQuestion(0);
+    setClickedOption(0);
+    setScore(0);
 }
 
-export default App
+  return (
+    <>
+      <h1 className="heading-txt">Quiz App</h1>
+      <div className="container">
+        {showResult ? (
+          <QuizResult score={score} totalScore={QuizData.length} tryAgain={resetAll}/>
+        ) : (
+          <>
+            <div className="question">{QuizData[currentQuestion].question}</div>
+
+            <div className="option-container">
+              {QuizData[currentQuestion].options.map((element, i) => {
+                return (
+                  <button
+                    className={`option-btn ${
+                      clickedOption == i + 1 ? "checked" : null
+                    }`}
+                    key={i}
+                    onClick={() => setClickedOption(i + 1)}
+                  >
+                    {element}
+                  </button>
+                );
+              })}
+            </div>
+            <input type="button" value="Next" id="next-button" onClick={clickAction}>
+              
+            </input>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
